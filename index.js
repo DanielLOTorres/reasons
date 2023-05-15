bf80f5390bbb8b325129b39af399a6fe094f9c8b
Daniel Torres <daniellourenco30@gmail.com>
2023-05-14 22:27:28 -0300

    primeiro

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+// Adicione o seguinte trecho para permitir o uso da porta 3000 em desenvolvimento
+if (process.env.NODE_ENV !== 'production') {
+  server.listen(3000, () => {
+    console.log('JSON Server is running on http://localhost:3000')
+  })
+}
+
+module.exports = server
