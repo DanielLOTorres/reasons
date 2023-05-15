7810b3ef9a95e340ac667f62287299095a58564a
Daniel Torres <daniellourenco30@gmail.com>
2023-05-15 00:58:02 -0300

    aaaaaaa

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 
-// Adicione o seguinte trecho para permitir o uso da porta 3000 em desenvolvimento
-if (process.env.NODE_ENV !== 'production') {
-  server.listen(3000, () => {
-    console.log('JSON Server is running on http://localhost:3000')
-  })
-}
+// // Adicione o seguinte trecho para permitir o uso da porta 3000 em desenvolvimento
+// if (process.env.NODE_ENV !== 'production') {
+//   server.listen(3000, () => {
+//     console.log('JSON Server is running on http://localhost:3000')
+//   })
+// }
 
-module.exports = server
+server.use("/", (req, res) => {
+  res.json({ message: "aasdnikolabdiNO" });
+});
+
+module.exports = server;
